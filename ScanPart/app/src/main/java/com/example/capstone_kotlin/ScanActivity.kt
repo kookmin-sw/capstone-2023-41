33c433c6c91999250eea4c6dd3db544e401cc434
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-03-23 20:58:23 +0900

    gradle : implementation 'com.github.yuriy-budiyev:code-scanner:2.3.2' 추가 manifests : user-permission - CAMERA layout : main -> RelativeLayout, new scan activity 전체를 초기화 하고 사용하는 언어를 kotlin으로 사용할 Implementation을 변경하였습니다. 사실상 기존과 다른 점은 언어와 implementation 만 이라고 생각하셔도 무방합니다.

---
+++ ScanPart/app/src/main/java/com/example/capstone_kotlin/ScanActivity.kt
@@ -0,0 +1,4 @@
+package com.example.capstone_kotlin
+
+class ScanActivity {
+}
--- ScanPart/app/src/main/java/com/example/scannerexample/CaptureAct.java
+++
@@ -1,6 +0,0 @@
-package com.example.scannerexample;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct extends CaptureActivity {
-}
