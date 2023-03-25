8c69a78411f26419dc8087a74f55e06a275fe0a2
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-03-25 20:31:49 +0900

    2023/03/24 금 대면 회의에서 구상된 스토리 보드에 맞게 기존에 사용하던 1회 스캔 방식을 사용하도록 변경.
    위 과정을 처리하면서 CaptureActivity는 따로 사용하지 않도록 함. 필요하다면 다시 추가할 예정 있음.

--- ScanPart/app/src/main/java/com/example/capstone_kotlin/ScanActivity.kt
+++ ScanPart/app/src/main/java/com/example/capstone_kotlin/ScanActivity.kt
@@ -1,4 +1,59 @@
 package com.example.capstone_kotlin
 
-class ScanActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
+
+class ScanActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+        val scanBtn = findViewById<Button>(R.id.scanBtn) as Button
+        scanBtn.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        scanCode()
+    }
+
+    private fun scanCode() {
+
+        val integrator = IntentIntegrator(this)
+        //integrator.captureActivity = CaptureActivity::class.java
+        integrator.setOrientationLocked(false) //
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("Scanning Code")
+
+        // change to front-camera
+        integrator.setCameraId(1)
+        integrator.initiateScan()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents != null) {
+                val builder = AlertDialog.Builder(this)
+                builder.setMessage(result.contents)
+                builder.setTitle("Scanning Result")
+                builder.setPositiveButton(
+                    "Scan Again"
+                ) { dialogInterface, i -> scanCode() }.setNegativeButton(
+                    "finish"
+                ) { dialogInterface, i -> finish() }
+                val dialog = builder.create()
+                dialog.show()
+            } else {
+                Toast.makeText(this, "No Results", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 }
