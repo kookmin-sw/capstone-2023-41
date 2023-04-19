34e6c70b3a078f99969956d2e8eb48242d1bb57e
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-04-19 20:40:53 +0900

    지도 위에 Pin 표시하는 기능 및 예제 첨부

--- ShinPart/app/src/main/java/com/example/capstone_kotlin/CoordDBHelper.kt
+++
@@ -1,5 +0,0 @@
-package com.example.capstone_kotlin
-
-class CoordDBHelper {
-
-}
--- ShinPart/app/src/main/java/com/example/capstone_kotlin/MapActivity.kt
+++ ShinPart/app/src/main/java/com/example/capstone_kotlin/MapActivity.kt
@@ -1,12 +1,10 @@
 package com.example.capstone_kotlin
 
-import android.R.id
-import android.net.Uri
+import android.R.*
+import android.graphics.*
 import android.os.Bundle
-import android.view.GestureDetector
+import android.view.*
 import android.view.GestureDetector.SimpleOnGestureListener
-import android.view.MotionEvent
-import android.view.View
 import android.view.View.OnTouchListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,20 +13,42 @@ import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
 
 
 class MapActivity : AppCompatActivity() {
-    var imageView: SubsamplingScaleImageView? = null
+    var imageView: PinView? = null
     var gestureDetector: GestureDetector? = null
+
+    // 테스트용
     val array = arrayOf(100, 100)
+    var test_count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        imageView = findViewById<View>(R.id.imageView) as SubsamplingScaleImageView
+        imageView = findViewById(R.id.imageView)
         imageView?.setImage(ImageSource.resource(R.drawable.map))
 
+        // 아래는 이미지 터치에 따른 예제 코드임.
         gestureDetector = GestureDetector(this, object : SimpleOnGestureListener() {
             override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
-                val sCoord = imageView?.viewToSourceCoord(e.x, e.y)
-                check_area(sCoord!!.x, sCoord.y)
+
+                // 터치된 곳의 좌표 pointt
+                var pointt = imageView?.viewToSourceCoord(e.x, e.y)
+
+                // 처음 터치시에 대한 이벤트 예제
+                if(test_count==0)
+                {
+                    test_count = 1
+                    imageView?.setPin(pointt)
+
+                    var s = (imageView?.minScale!! + imageView?.maxScale!!)/3
+                    imageView?.setScaleAndCenter(s!!, pointt)
+                    //AnimationBuilder(s!!, pointt).withD
+                }
+                else
+                {
+                    imageView?.addPin(pointt, 0, R.drawable.duck)
+                }
+                check_area(pointt!!.x, pointt.y)
                 return true
             }
         })
@@ -39,10 +59,12 @@ class MapActivity : AppCompatActivity() {
         })
     }
 
+    // 입력된 x,y 좌표 값에 대한 처리 함수 예제
     private fun check_area(x: Float, y: Float)
     {
         if (x < array[0] && y<array[1])
         {
+            imageView?.clearPin()
             val msg = "x:" + x + "y:" + y
             Toast.makeText(applicationContext, msg, Toast.LENGTH_SHORT).show()
         }
