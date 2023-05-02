5fcc91b858478a268afd08e9cc6dd3148ffa6686
Gnoyh <4ksenhs@gmail.com>
2023-05-02 19:31:05 +0900

    이미지 추가

--- Sqlite/app/src/main/java/com/example/sqlite/DataBaseHelper.kt
+++ Sqlite/app/src/main/java/com/example/sqlite/DataBaseHelper.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import java.io.FileOutputStream
 import java.io.IOException
 
@@ -25,19 +27,20 @@ class DataBaseHelper(private val context: Context) :
 
     private fun checkDatabase(): Boolean {
         val dbFile = context.getDatabasePath(DATABASE_NAME)
+
         return dbFile.exists()
     }
 
     private fun copyDatabase() {
         try {
             val folder = context.getDatabasePath(DATABASE_NAME).parentFile
+
             if (!folder.exists()) {
                 folder.mkdir()
             }
 
             val inputStream = context.assets.open(DATABASE_NAME)
             val outputStream = FileOutputStream(dbPath + DATABASE_NAME)
-
             val buffer = ByteArray(1024)
             var length: Int
 
@@ -54,8 +57,10 @@ class DataBaseHelper(private val context: Context) :
         }
     }
 
-    data class PlaceNode(val idx: Int, val id: Int, val name: String, val x: Int, val y: Int, val x1: Int, val y1: Int, val x2: Int, val y2: Int)
-    data class CrossNode(val idx: Int, val id: Int, val x: Int, val y: Int, val x1: Int, val y1: Int, val x2: Int, val y2: Int)
+    data class PlaceNode(val idx: Int, val id: Int, val name: String, val x: Int, val y: Int,
+                         val x1: Int, val y1: Int, val x2: Int, val y2: Int, val img1: Bitmap?, val img2: Bitmap?)
+    data class CrossNode(val idx: Int, val id: Int, val x: Int, val y: Int, val nodes: List<Triple<Int, Int, String>>,
+                         val imgEast: Bitmap?, val imgWest: Bitmap?, val imgSouth: Bitmap?, val imgNorth: Bitmap?)
 
     val placeList = mutableListOf<PlaceNode>()
     val crossList = mutableListOf<CrossNode>()
@@ -72,7 +77,13 @@ class DataBaseHelper(private val context: Context) :
                 val x = it.getInt(3)
                 val y = it.getInt(4)
 
-                placeList.add(PlaceNode(idx, id, name, x, y, x - 15, y - 15, x + 15, y + 15))
+                val bytes1: ByteArray = it.getBlob(5)
+                val bytes2: ByteArray = it.getBlob(6)
+
+                val img1: Bitmap? = BitmapFactory.decodeByteArray(bytes1, 0, bytes1.size)
+                val img2: Bitmap? = BitmapFactory.decodeByteArray(bytes2, 0, bytes2.size)
+
+                placeList.add(PlaceNode(idx, id, name, x, y, x - 15, y - 15, x + 15, y + 15, img1, img2))
             }
 
             it.close()
@@ -91,8 +102,41 @@ class DataBaseHelper(private val context: Context) :
                 val id = it.getInt(1)
                 val x = it.getInt(2)
                 val y = it.getInt(3)
+                val nodeList = it.getString(4)
 
-                crossList.add(CrossNode(idx, id, x, y, x - 15, y - 15, x + 15, y + 15))
+                val bytesEast: ByteArray = it.getBlob(5)
+                val bytesWest: ByteArray = it.getBlob(6)
+                val bytesSouth: ByteArray? = it.getBlob(7)
+                val bytesNorth: ByteArray? = it.getBlob(8)
+
+                val nodes = nodeList.split(",")
+                    .map { tripleString ->
+                        val (id, distance, direction) = tripleString.split("_")
+                        Triple(id.toInt(), distance.toInt(), direction)
+                    }
+
+                var imgEast: Bitmap? = null
+                var imgWest: Bitmap? = null
+                var imgSouth: Bitmap? = null
+                var imgNorth: Bitmap? = null
+
+                if (bytesEast != null) {
+                    imgEast = BitmapFactory.decodeByteArray(bytesEast, 0, bytesEast.size)
+                }
+
+                if (bytesWest != null) {
+                    imgWest = BitmapFactory.decodeByteArray(bytesWest, 0, bytesWest.size)
+                }
+
+                if (bytesSouth != null) {
+                    imgSouth = BitmapFactory.decodeByteArray(bytesSouth, 0, bytesSouth.size)
+                }
+
+                if (bytesNorth != null) {
+                    imgNorth = BitmapFactory.decodeByteArray(bytesNorth, 0, bytesNorth.size)
+                }
+
+                crossList.add(CrossNode(idx, id, x, y, nodes, imgEast, imgWest, imgSouth, imgNorth))
             }
 
             it.close()
@@ -103,7 +147,7 @@ class DataBaseHelper(private val context: Context) :
 
     fun findID(x: Int, y: Int, list: List<PlaceNode>): Int {
         for (i in list) {
-            if ((i.x1 <= x && x <= i.x2) && (i.y1 <= y && y <= i.y2)) {
+            if ((i.x1 <= x && x <= i.x2) && (i.y1<= y && y <= i.y2)) {
                 return i.id
             }
         }
--- Sqlite/app/src/main/java/com/example/sqlite/MainActivity.kt
+++ Sqlite/app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.sqlite
 
 import android.os.Bundle
+import android.graphics.BitmapFactory
 import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private lateinit var imageView1: ImageView
+    private lateinit var imageView2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,25 +20,31 @@ class MainActivity : AppCompatActivity() {
 
         val db = DataBaseHelper(this)
 
-        val list1 = db.getNodesPlace()
-        val list2 = db.getNodesCross()
+        val nodesPlace = db.getNodesPlace()
+        val nodesCross = db.getNodesCross()
 
         var text = "list1\n\n"
 
-        for (i in list1) {
+        for (i in nodesPlace) {
             text += "${i.idx}, ${i.id}, ${i.name}, ${i.x}, ${i.y}, ${i.x1}, ${i.y1}, ${i.x2}, ${i.y2}\n"
         }
 
         text += "\nlist2\n\n"
 
-        for (i in list2) {
-            text += "${i.idx}, ${i.id}, ${i.x}, ${i.y}, ${i.x1}, ${i.y1}, ${i.x2}, ${i.y2}\n"
+        for (i in nodesCross) {
+            text += "${i.idx}, ${i.id}, ${i.x}, ${i.y}, ${i.nodes}\n"
         }
 
-        text += "\nid = ${db.findID(920, 440, list1)}\n"
+        text += "\nid = ${db.findID(920, 440, nodesPlace)}\n"
 
-        text += "\nx = ${db.findXY(464, list1)?.first}, y = ${db.findXY(464, list1)?.second}"
+        text += "\nx = ${db.findXY(464, nodesPlace)?.first}, y = ${db.findXY(464, nodesPlace)?.second}"
 
         textView.text = text
+/*
+        imageView1 = findViewById(R.id.iv_img1)
+        imageView2 = findViewById(R.id.iv_img2)
+
+        imageView1.setImageBitmap(nodesPlace[1].img2)
+        imageView2.setImageBitmap(nodesCross[2].imgNorth)*/
     }
 }
