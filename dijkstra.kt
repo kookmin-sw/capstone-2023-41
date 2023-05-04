d98dcd9f6280a0da515f00be20a3c8e3f52cf2c1
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-05-04 16:30:51 +0900

    인덱싱 문제 수정

--- dijkstra.kt
+++ dijkstra.kt
@@ -1,15 +1,20 @@
 import java.util.*
 
-// near : 인접 노드, weight : 거리
-class Edge(val near: Int, val weight: Int)
+// now : 현재 노드, near : 인접 노드, weight : 거리
+class Edge(val now: Int, val near: Int, val weight: Int)
 
 // graph : 전체 노드(각 노드 별로 인접 노드와 거리 가짐)
 // start : 시작 노드
 // end : 도착 노드
 fun dijkstra(graph: Array<ArrayList<Edge>>, start: Int, end: Int): Pair<Int, List<Int>> {
 
+    val graph2 = Array(1000) { arrayListOf<Edge>() }
+    for (i in graph.indices){
+        graph2[graph[i][0].now] = graph[i]
+    }
+
     // dist : 최단 거리 담을 배열. 초기엔 max_value 초기화
-    val dist = IntArray(graph.size) { Int.MAX_VALUE }
+    val dist = IntArray(1000) { Int.MAX_VALUE }
     dist[start] = 0 // 시작 노드는 거리를 0으로.
 
     // 가장 짧은 노드 선택하기 위해 우선순위 큐
@@ -17,7 +22,7 @@ fun dijkstra(graph: Array<ArrayList<Edge>>, start: Int, end: Int): Pair<Int, Lis
     pq.add(start to 0) // 출발노드 
     
     // prev : 최단 경로상에서 현재 노드의 직전 노드 기록
-    val path = IntArray(graph.size) { -1 }
+    val path = IntArray(1000) { -1 }
 
     while (pq.isNotEmpty()) {
         // poll : 우선순위 큐에서 헤드(= 가장 짧은 거리의 노드) 추출
@@ -26,6 +31,7 @@ fun dijkstra(graph: Array<ArrayList<Edge>>, start: Int, end: Int): Pair<Int, Lis
         // 도착 노드에 도달하면 현 최단거리 리턴하고 종료
         if (cur == end) {
             val path2 = ArrayList<Int>()
+
             path2.add(end)
             while(true){
                 path2.add(path[path2.last()])
@@ -37,7 +43,7 @@ fun dijkstra(graph: Array<ArrayList<Edge>>, start: Int, end: Int): Pair<Int, Lis
         }
 
         // 현재 노드를 기준으로 인접노드들 불러옴.
-        for (edge in graph[cur]) {
+        for (edge in graph2[cur]) {
             // 시작노드 ~ 현재노드 거리 + 현재노드 ~ 인접노드 거리
             // 거리 비교 후 더 짧은 걸로 갱신
             val newDist = curDist + edge.weight
@@ -55,16 +61,16 @@ fun dijkstra(graph: Array<ArrayList<Edge>>, start: Int, end: Int): Pair<Int, Lis
 
 fun main() {
     val graph = arrayOf(
-        arrayListOf(Edge(2, 10)),
-        arrayListOf(Edge(3,13)),
-        arrayListOf(Edge(0,10), Edge(3,5), Edge(4,11)),
-        arrayListOf(Edge(1,13),Edge(2,5), Edge(5,11)),
-        arrayListOf(Edge(2,11),Edge(5,6)),
-        arrayListOf(Edge(3,11),Edge(4,6),Edge(6,10)),
-        arrayListOf(Edge(5,10))
+        arrayListOf(Edge(400, 402, 10)),
+        arrayListOf(Edge(401, 403,13)),
+        arrayListOf(Edge(402, 400,10), Edge(402,403,5), Edge(402,404,11)),
+        arrayListOf(Edge(403,401,13),Edge(403,402,5), Edge(403,405,11)),
+        arrayListOf(Edge(404,402,11),Edge(404,405,6)),
+        arrayListOf(Edge(405,403,11),Edge(405,404,6),Edge(405,406,10)),
+        arrayListOf(Edge(406,405,10))
     )
-    val start = 0
-    val end = 6
+    val start = 400
+    val end = 406
     val (dist, path) = dijkstra(graph, start, end)
     if (dist == -1) {
         println("error")
