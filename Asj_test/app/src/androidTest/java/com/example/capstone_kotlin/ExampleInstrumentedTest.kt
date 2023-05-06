13c2f634635d1d2123787f9263a5cd938e9450bd
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-05-06 15:51:35 +0900

    QR 촬영 후 갑 받아오기

--- Asj_test/app/src/androidTest/java/com/example/capstone_kotlin/ExampleInstrumentedTest.kt
+++ Asj_test/app/src/androidTest/java/com/example/capstone_kotlin/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.asj_test
+package com.example.capstone_kotlin
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.asj_test", appContext.packageName)
+        assertEquals("com.example.capstone_kotlin", appContext.packageName)
     }
 }
