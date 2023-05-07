fcad06ebecec18ec6f378b4de846b10c868ae49a
anseongjun <dkstjdwns@kookmin.ac.kr>
2023-05-08 00:11:15 +0900

    자주스 info 활성화

--- Asj_test/app/src/main/java/com/example/capstone_kotlin/MainActivity.kt
+++ Asj_test/app/src/main/java/com/example/capstone_kotlin/MainActivity.kt
@@ -3,11 +3,17 @@ import android.app.Activity
 import android.content.Intent // Intent는 액티비티간 데이터를 전달하는데 사용된다.
 import android.graphics.Color
 import android.graphics.PointF
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity // AppCompatActivity 클래스를 임포트. AppCompatActivity는 안드로이드 앱에서 사용되는 기본 클래스
 import android.os.Bundle // Bundle은 액티비티가 시스템에서 재생성될 때 데이터를 저장하고 다시 가져오는 데 사용
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.view.View
 import android.widget.*
+import com.davemorrissey.labs.subscaleview.ImageSource
+//import androidx.lifecycle.viewmodel.CreationExtras.Empty.map
+import com.example.capstone_kotlin.DataBaseHelper
 import com.google.zxing.integration.android.IntentIntegrator.REQUEST_CODE
 
 // Activity 는 사용자와 상호작용 하기 위한 하나의 단위
@@ -17,15 +23,29 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
 
     // 테스트위해서 lateinit 설정
     private lateinit var searchView1: SearchView
+    private lateinit var text1: TextView
+    private lateinit var text2: TextView
+    private lateinit var map: PinView
 
-    private var isButtonsVisible = false
+    var gestureDetector: GestureDetector? = null
+
+    var db: DataBaseHelper? = null
+    var nodesPlace: List<DataBaseHelper.PlaceNode>? = null
+    var nodesCross: List<DataBaseHelper.CrossNode>? = null
+    var ratio: Float? = null
 
     override fun onCreate(savedInstanceState: Bundle?) { // onCreate 함수를 오버라이드. 이 함수는 액티비티가 생성될 때 호출됨.
         super.onCreate(savedInstanceState) // 부모 클래스의 onCreate 함수를 호출
         setContentView(R.layout.activity_main)
 
+        // DB
+        db = DataBaseHelper(this)
+        nodesPlace = db!!.getNodesPlace()
+        nodesCross = db!!.getNodesCross()
+
         // 지도
-        val map = findViewById<PinView>(R.id.map)
+        map = findViewById<PinView>(R.id.map)
+        map.setImage(ImageSource.resource(R.drawable.mirae_4f))
 
         // 출발지, 목적지 입력 서치뷰
         searchView1 = findViewById<SearchView>(R.id.searchView1)
@@ -44,12 +64,40 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
         var infoPic1 = findViewById<ImageView>(R.id.infoPic1)
         var infoPic2 = findViewById<ImageView>(R.id.infoPic2)
 
+        // 정보창에 띄울 이름과 접근성
+        text1 = findViewById(R.id.text1)
+        text2 = findViewById(R.id.text2)
+
+        // 화면 비율
+        ratio = map?.getResources()!!.getDisplayMetrics().density.toFloat() // 화면에 따른 이미지의 해상도 비율
+
+        text1.setText("${nodesPlace!![0].x} (${nodesPlace!![0].y}호)")
+
+        if(nodesPlace!![0].access == 0){
+            text2.setBackgroundColor(Color.RED)
+        }
+        else if(nodesPlace!![0].access == 1){
+            text2.setBackgroundColor(Color.YELLOW)
+        }
+        else{
+            text2.setBackgroundColor(Color.GREEN)
+        }
+
+//        infoPic1.setImageBitmap(nodesPlace!![0].img1)
+//        infoPic2.setImageBitmap(nodesPlace!![0].img2)
+
         // 출발, 도착 버튼
         var start = findViewById<Button>(R.id.start)
         var end = findViewById<Button>(R.id.end)
 
+        // 지도 첫 크기
+//        map.scaleX = 2f
+//        map.scaleY = 2f
+//        map.setScaleAndCenter(2f, map.center)
+//        map.animateScaleAndCenter(2f, map.center)
+
         // 지도 크기 제한
-        map.maxScale = 0.5f
+        map.maxScale = 1f
 
         // QR 촬영 버튼 활성화.
         qrButton.setOnClickListener{
@@ -78,9 +126,10 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
                 if (query.isEmpty() && searchView2.query.isEmpty()) {
                     searchView2.visibility = View.GONE
                 } else {
-                    if(query == "123"){
+                    // QR 로 데이터 받아오거나 DB에 있는 지명 검색 완료 시
+                    // 정보창 보이게 함. 123은 테스트.
+                    if(query == "123123123"){
                         info.visibility = View.VISIBLE
-                        isButtonsVisible = true
                     }
                     searchView2.visibility = View.VISIBLE
                 }
@@ -117,65 +166,41 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
         // 출발 버튼 누르면 searchView1 채우기
         start.setOnClickListener{
             searchView1.setQuery("test", true)
+            info.visibility = View.GONE
         }
 
         // 도착 버튼 누르면 searchView2 채우기
         end.setOnClickListener{
             searchView2.setQuery("test2", true)
+            info.visibility = View.GONE
         }
 
-        var touchTime = 0L
+        // 특정 좌표(현재 자주스)를 누를 때만 정보창 활성화.
+        gestureDetector = GestureDetector(this, object : SimpleOnGestureListener() {
+            override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+                var pointt = map?.viewToSourceCoord(e.x, e.y)
 
-        map.setOnTouchListener { _, event ->
-            when(event.actionMasked) {
-                MotionEvent.ACTION_DOWN -> {
-                    if(info.visibility == View.VISIBLE){
-                        info.visibility = View.GONE
-                    }
-                    else{
-                        info.visibility = View.VISIBLE
-                    }
+                if((pointt!!.x/ratio!! > 900 && pointt!!.x/ratio!! < 1000) && (pointt!!.y/ratio!! > 400 && pointt!!.y/ratio!! < 500)){
+                    infoPic1.setImageBitmap(nodesPlace!![0].img1)
+                    infoPic2.setImageBitmap(nodesPlace!![0].img2)
+                    info.visibility = View.VISIBLE
                 }
+                else{
+                    info.visibility = View.GONE
+                }
+
+
+                return true
             }
-            false
-        }
+        })
 
         // 클릭 위치마다 띄우는 사진을 달리하기 위한 테스트용 함수
-//        map.setOnTouchListener { view, event ->
-//            when (event.action) {
-//                MotionEvent.ACTION_DOWN -> {
-//                    val x = event.x
-//                    val y = event.y
-//                    val outPoint = IntArray(2)
-//                    map.getLocationOnScreen(outPoint)
-//                    val imageX = x - outPoint[0]
-//                    val imageY = y - outPoint[1]
-//                    // 클릭한 좌표값 사용하기
-////                    Toast.makeText(this, "x: $imageX, y: $imageY", Toast.LENGTH_SHORT).show()
-//                    if (imageX < 500f && imageY < 200f){
-//                        if (isButtonsVisible) {
-//                            info.visibility = View.GONE
-//                        } else {
-//                            info.visibility = View.VISIBLE
-//                        }
-//                        isButtonsVisible = !isButtonsVisible
-//                        infoPic1.setImageResource(R.drawable.pushpin_blue)
-//                        infoPic2.setImageResource(R.drawable.pushpin_blue)
-//                    }
-//                    else if(imageX < 1000f && imageY < 500f){
-//                        if (isButtonsVisible) {
-//                            info.visibility = View.GONE
-//                        } else {
-//                            info.visibility = View.VISIBLE
-//                        }
-//                        isButtonsVisible = !isButtonsVisible
-//                        infoPic1.setImageResource(R.drawable.duck)
-//                        infoPic2.setImageResource(R.drawable.duck)
-//                    }
-//                }
-//            }
-//            false
-//        }
+
+        map.setOnTouchListener { view, event ->
+            gestureDetector!!.onTouchEvent(
+                event
+            )
+        }
 
         // 스피너 활성화
         val adapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, items)
@@ -194,6 +219,7 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
 
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
+
     }
 
     // QR 촬영 후 데이터 값 받아옴.
@@ -205,6 +231,27 @@ class MainActivity : AppCompatActivity() {  // MainActivity정의, AppCompatActi
             val returnedData = data?.getStringExtra("QRdata")
             Toast.makeText(this, returnedData, Toast.LENGTH_SHORT).show()
             searchView1.setQuery(returnedData, false)
+
         }
+    }
+
+    // 입력된 x,y 좌표 값에 대한 처리 함수 예제
+    private fun check_area(x: Float, y: Float)
+    {
+        var testX = x/ratio!!
+        var testY = y/ratio!!
+
+        var msg2 = testX.toString() + ":" + testY.toString()
+        Toast.makeText(applicationContext, msg2, Toast.LENGTH_SHORT).show()
+        var id = db!!.findPlacetoXY(testX.toInt(), testY.toInt(), nodesPlace!!)
+        if (id != null)
+        {
+            map?.addPin(PointF(id!!.x.toFloat()*ratio!!, id!!.y.toFloat()*ratio!!), 1, R.drawable.pushpin_blue)
+        }
+        // 핀 지우기 예제
+//        if ( 'condition' )
+//        {
+//            imageView?.clearPin()
+//        }
     }
 }
